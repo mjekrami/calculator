7e706aa7e90d5a785c24e82c502551a58bcd01c2
mjekrami <mjavad.ekrami@gmail.com>
2024-01-26 23:53:08 +0330

    added junit test, controller and service

---
+++ src/test/java/com/mjekrami/calculator/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.mjekrami.calculator;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    private Calculator calculator;
+
+    @Test
+    public void testSum() {
+        assertEquals(5,calculator.sum(2,3));
+    }
+}
